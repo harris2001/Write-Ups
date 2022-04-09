962a3bc75c2828654c17de6300c953baaa4362b4
harris2001 <harrishadjiantonis@gmail.com>
2022-04-09 22:06:37 +0100

    Add files via upload

---
+++ Misc/Misc/i_c_u/src/secret.rs
@@ -0,0 +1,9 @@
+// mod secret {
+//     pub(crate) fn FLAG(){
+//         let FLAG_TOO = "tesetingGG";
+//         println!(FLAG_TOO);
+//     }
+//     fn main() {
+//         FLAG();
+//     }
+// }
--- Misc/Misc/i_c_u/src/solve.py
+++ Misc/Misc/i_c_u/src/solve.py
@@ -1,1 +1,65 @@
+import base64
+from PIL import Image
 from pwn import *
+import subprocess
+from subprocess import check_output
+
+
+def printing(image):
+    image_64 = base64.b64encode(image)
+    print(str(image_64)[2:-1])
+    # print("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n")
+im1 = Image.open("../img1.png")
+pixel1 = im1.load()
+im2 = Image.open("../img2.png")
+pixel2 = im2.load()
+for i in range(im1.size[0]):
+    for j in range(im1.size[1]):
+        pix1 = (pixel1[i,j])
+        pix2 = (pixel2[i,j])
+        pixel2[i,j] += int(pix1/18)
+
+im2.save('../img2-modified.png')
+
+im1 = Image.open("../img1.png")
+pixel1 = im1.load()
+im2 = Image.open("../img2.png")
+pixel2 = im2.load()
+for i in range(im1.size[0]):
+    for j in range(im1.size[1]):
+        pix1 = (pixel1[i,j])
+        pix2 = (pixel2[i,j])
+        pixel1[i,j] = pixel2[i,j] + int(pix1/13)
+
+im1.save('../img1-modified.png')
+
+image1 = open("../img1-modified.png", 'rb').read()
+image2 = open("../img2-modified.png", 'rb').read()
+
+printing(image1)
+printing(image2)
+
+
+# r = remote("icu.chal.pwni.ng",1337)
+
+# comm = str(r.recv())[2:-3].split(" ")[2]
+# print(">>>>>"+comm)
+# hashcash = check_output(["hashcash", "-qmb24", comm])
+# out = hashcash
+# print("Sending ")
+# print(out)
+#
+# r.send(hashcash)
+# out = r.recv()
+# print(f"[Received]:{out}")
+# print(f"[Sending]:{base64.b64encode(image1)}")
+# r.send(base64.b64encode(image1))
+# r.send(b'\n')
+# out = r.recv()
+# print(out)
+# print(f"[Sending]:{base64.b64encode(image2)}")
+# r.send(base64.b64encode(image2))
+# r.send('\n')
+# r.interactive()
+
+
